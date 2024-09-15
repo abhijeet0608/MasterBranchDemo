da07b9f47c6ea5dbbf547355bf044fe32a8763d5
Abhijeet0608 <abhijeetgavali7297@gmail.com>
2024-09-15 17:27:29 +0530

    first commit

---
+++ SeleniumPractice/src/Test/SelTest1.java
@@ -0,0 +1,20 @@
+package Test;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class SelTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhij\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+driver.get("https://rahulshettyacademy.com/locatorspractice");
+
+
+
+
+
+}
+}
